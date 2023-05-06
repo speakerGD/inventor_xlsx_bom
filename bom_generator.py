bddf1c6c1a9cd974d24a114fd8473158d4791f46
speakerGD <101511698+speakerGD@users.noreply.github.com>
2023-05-06 22:57:24 +0300

    first working version

--- bom_generator.py
+++ bom_generator.py
@@ -6,46 +6,7 @@ import pprint
 import operator
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
-
-
-PART_NUMBER = "Обозначение"
-BOM_STRUCTURE = "Структура спецификации"
-QUANTITY = "КОЛ."
-DESCRIPTION = "Наименование"
-MATERIAL = "Материал"
-MASS = "Масса"
-STOCK_NUMBER = "Инвентарный номер"
-PROJECT = "Проект"
-VENDOR = "Поставщик"
-FILE_PATH = "Путь файла"
-CUSTOM_MASS = "Mass"
-CUSTOM_LENGTH = "Length"
-CUSTOM_WIDTH = "Width"
-CUSTOM_AREA = "Area"
-
-PROFILE_MATERIAL = "ПрофильныйПрокат"
-FLAT_MATERIAL = "ПлоскийПрокат"
-PURCHASED = "ПокупныеИзделия"
-MD1000 = "УнифицированныеИзделия"
-
-FLAT_MATERIAL_PREFIX = (
-    "Лист",
-    "Пластина",
-    "Плита",
-    "Профнастил",
-    "Фанера",
-    "Сетка",
-    "Сэндвич-панель",
-)
-
-MATERIAL_TYPE = {
-    "Ст3сп": ["Ст3", "ст3"],
-    "08Х18Н10": ["08Х18Н10", "нерж"],
-    "ЛС59-1": ["ЛС59", "латунь"],
-    "Ф-4": ["Ф-4", "фторопласт"],
-    "маслонаполненный ПА-6": ["капролон"],
-    "08пс": ["08пс"],
-}
+from settings import *
 
 
 def main():
